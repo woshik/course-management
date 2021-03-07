15de80137ce8f9a17e48a472c48f0cc405a41aca
Anik <woshikuzzaman@gmail.com>
2021-03-07 20:27:32 +0600

    # Fix - path issue fix

--- server/index.js
+++ server/index.js
@@ -4,11 +4,11 @@ const { resolve } = require('path');
 const server = express();
 
 // Serve any static files
-server.use(express.static(resolve(__dirname, '../client/build')));
+server.use(express.static(resolve(__dirname, '../client/dist')));
 
 // Handle React routing, return all requests to React app
 server.get('*', (req, res) => {
-	res.sendFile(resolve(__dirname, '../client/build/index.html'));
+	res.sendFile(resolve(__dirname, '../client/dist/index.html'));
 });
 
 server.listen(5000, () => console.log('Server is listening on port 5000'));
