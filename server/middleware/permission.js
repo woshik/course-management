204e61adf13fddba8d00be1514999d71b4c45efc
anik <anik@w3engineers.com>
2021-04-19 01:51:24 +0600

    # Info   - production build

--- server/middleware/permission.js
+++ server/middleware/permission.js
@@ -1,5 +1,5 @@
 module.exports = (roleName) => (req, res, next) => {
-  if (req.user.role === roleName) {
+  if (req?.user?.role === roleName) {
     next();
   } else {
     res.status(401).send({ message: 'Invalid permission' });
