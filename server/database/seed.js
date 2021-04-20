d04d34cdb8667b04b94f38d6ab92dcfa2fad99ee
anik <anik@w3engineers.com>
2021-04-20 11:50:03 -0600

    # Feature   - seed file update provide a dynamic email address

--- server/database/seed.js
+++ server/database/seed.js
@@ -3,23 +3,28 @@ const { connectWithMongodb, getDB } = require('./connection');
 require('dotenv').config();
 
 connectWithMongodb(async () => {
-  const user = await getDB().collection('users');
-  const data = await user.findOne({
-    email: 'test@test.com',
-  });
-
-  if (data) {
-    console.log('\n\ntest admin already exist in database\n\n');
-  } else {
-    const newUser = await user.insertOne({
-      fullName: 'Test',
+  const email = process.argv[2];
+  if (email) {
+    const user = await getDB().collection('users');
+    const data = await user.findOne({
       email: 'test@test.com',
-      password: await bcrypt.hash('12345678', await bcrypt.genSalt(10)),
-      role: 'admin',
     });
 
-    console.log(newUser);
-  }
+    if (data) {
+      console.log('\n\ntest admin already exist in database\n\n');
+    } else {
+      const newUser = await user.insertOne({
+        fullName: 'Test',
+        email: 'test@test.com',
+        password: await bcrypt.hash('12345678', await bcrypt.genSalt(10)),
+        role: 'admin',
+      });
 
-  process.exit(1);
+      console.log(newUser);
+    }
+
+    process.exit(1);
+  } else {
+    console.log('Please, provide a mail address');
+  }
 });
